9fa3ce65da5d9e5abdd7d326ad7ed580ac1f72ab
hugo meiland <hugo.meiland@microsoft.com>
2018-07-09 13:32:24 +0200

    adding blob and file mount scripts

---
+++ general/specs/blobmount/cluster-init/scripts/000_install_scripts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#copy scripts
+sudo cp ../files/mount_azure_blob /bin/
+sudo chmod 755 /bin/mount_azure_blob 
+
+sudo cp ../files/mount_azure_file /bin/
+sudo chmod 755 /bin/mount_azure_file 
